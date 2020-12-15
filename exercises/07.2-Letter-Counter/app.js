0922abaf42dd9c10712b707156a7fd3c501f1893
kedwards413 <kaela.rose.edwards@gmail.com>
2020-12-15 00:36:51 +0000

    practice

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,13 +1,13 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 let counts = {};
-
+par = par.toLocaleLowerCase();
 // your code here
 for (let i=0; i < par.length; i++){
     if (par[i] != " "){
         if(par[i] in counts){
-            counts[par[i]]+= 1;
-            console.log(par[i]);
-            console.log(i);
+            counts[par[i]]+= 1;  
+        } else {
+            counts[par[i]] = 1;
         }
     }
 }
