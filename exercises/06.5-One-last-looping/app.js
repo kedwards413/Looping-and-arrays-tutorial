d9c873c7766caf1fedfee63eca70f3e06ce23f18
kedwards413 <kaela.rose.edwards@gmail.com>
2020-12-15 19:55:46 +0000

    more practice

--- exercises/06.5-One-last-looping/app.js
+++ exercises/06.5-One-last-looping/app.js
@@ -1,3 +1,6 @@
 let mySampleArray = ['Esmeralda', 'Kiko', 'Ruth', 'Lebron', 'Pedro', 'Maria', 'Lou', 'Fernando', 'Cesco', 'Bart', 'Annie'];
-
+for (let i = mySampleArray.length-1; i>=0; i = i-1){
+    let item = mySampleArray[i];
+    console.log(item);
+}
 //your code here
