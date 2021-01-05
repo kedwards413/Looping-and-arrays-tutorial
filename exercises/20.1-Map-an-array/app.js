09ef334ce87495d7e8131354c4675a2a8f687b04
kedwards413 <kaela.rose.edwards@gmail.com>
2021-01-05 21:06:16 +0000

    still practicing

--- exercises/20.1-Map-an-array/app.js
+++ exercises/20.1-Map-an-array/app.js
@@ -1,9 +1,11 @@
 var arrayOfCelsiusValues = [-2,34,56,-10];
 
 var arrayOfFahrenheitValues = arrayOfCelsiusValues.map(function(value){
-	
-	//add your code here and return the new value
+	let convertedTemp = value*1.8+32;
+    //add your code here and return the new value
+    return convertedTemp;
 	
 });
+
 
 console.log(arrayOfFahrenheitValues);
